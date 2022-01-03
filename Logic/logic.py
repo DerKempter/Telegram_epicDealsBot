18f25792e9258682fc1088eccef718f6acfcabda
DerKempter <Joshkempter@gmail.com>
2022-01-03 18:31:57 +0100

    adjust Logic for new handler getter | first implementation of free_games_getter

--- Logic/logic.py
+++ Logic/logic.py
@@ -1,6 +1,8 @@
 import logging
-import Handlers.handlers as handlers
+from datetime import datetime
 
+import Handlers.handlers as handlers
+from epicstore_api import EpicGamesStoreAPI
 from telegram.ext import Updater
 
 
@@ -18,6 +20,7 @@ class BotLogic:
     updater = None
     dispatcher = None
     token_string = None
+    handler_list = None
 
     def __init__(self):
         setup_logging()
@@ -34,9 +37,10 @@ class BotLogic:
         self.updater = Updater(token=token_string, use_context=True)
         self.dispatcher = self.updater.dispatcher
 
-        self.dispatcher.add_handler(handlers.start_handler)
-        self.dispatcher.add_handler(handlers.test_handler)
-        self.dispatcher.add_handler(handlers.unknown_handler)
+        self.handler_list = handlers.get_handlers()
+        for handler in self.handler_list:
+            self.dispatcher.add_handler(handler)
+
         return self.dispatcher
 
     def get_dispatcher(self):
@@ -44,3 +48,40 @@ class BotLogic:
             return self.dispatcher
         else:
             return self.prep_dispatcher()
+
+    def get_free_games(self) -> []:
+        api = EpicGamesStoreAPI()
+        free_games = api.get_free_games()['data']['Catalog']['searchStore']['elements']
+        free_games_parsed = []
+        for game in free_games:
+            game_name = game['title']
+            game_thumb = None
+
+            for image in game['KeyImages']:
+                if image['type'] == 'Thumbnail':
+                    game_thumb = image['url']
+            game_price = game['price']['totalPrice']['fmtPrice']['originalPrice']
+            start_date = None
+            end_date = None
+            try:
+                game_promotions = game['promotions']['promotionalOffers']
+                upcoming_promotions = game['promotions']['upcomingPromotionalOffers']
+                if not game_promotions and upcoming_promotions:
+                    # Promotion is not active yet, but will be active soon.
+                    promotion_data = upcoming_promotions[0]['promotionalOffers'][0]
+                    start_date_iso, end_date_iso = (
+                        promotion_data['startDate'][:-1], promotion_data['endDate'][:-1]
+                    )
+                    # Remove the last "Z" character so Python's datetime can parse it.
+                    start_date = datetime.fromisoformat(start_date_iso)
+                    end_date = datetime.fromisoformat(end_date_iso)
+                    # Will be free from start_date to end_date
+                else:
+                    start_date = None
+                    end_date = None
+                    # Is free now
+            except TypeError:
+                pass
+            current_free_game = [game_name, game_thumb, game_price, start_date, end_date]
+            free_games_parsed.append(current_free_game)
+        return free_games_parsed
