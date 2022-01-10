0f04d91a314002d3e1cb824c76b17e5a12a8bc8f
DerKempter <Joshkempter@gmail.com>
2022-01-10 17:24:10 +0100

    Prepare to deploy on Heroku

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import logging
+import os
 from Handlers.handlers import get_bot
 from Logic.logic import BotLogic
+
+PORT = int(os.environ.get('PORT', 5000))
+TOKEN = os.environ["TOKEN"]
 
 main_bot = BotLogic()
 get_bot(main_bot)
