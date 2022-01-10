e382225d1368c69af9deccf290e7728dc7078a9c
DerKempter <Joshkempter@gmail.com>
2022-01-10 17:47:55 +0100

    Update main.py
    port listening

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import logging
+import os
+
 from Handlers.handlers import get_bot
 from Logic.logic import BotLogic
 
+PORT = int(os.environ.get('PORT', 5000))
 
 main_bot = BotLogic()
 get_bot(main_bot)
